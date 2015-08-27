d58756b9b7f784acab40e859c538b6d1a6b6e62a
Saharath Kleips <SaharathKleips@yahoo.com>
2015-08-26 21:33:19 -0700

    init commit

---
+++ PA0/README.md
@@ -0,0 +1,24 @@
+A Simple Pass Through Shader Example
+====================================
+
+Building This Example
+---------------------
+
+*This example requires GLUT and GLEW* 
+
+*On ubuntu they can be installed with this command*
+
+>$ sudo apt-get install freeglut3-dev freeglut3 libglew1.6-dev
+
+*On a Mac you can get these libraries with this command(using homebrew)*
+>$ brew install freeglut glew
+
+To build this example just 
+
+>$ cd build
+
+>$ make
+
+*If you are using a Mac you will need to edit the makefile in the build directory*
+
+The excutable will be put in bin
---
+++ PA0/src/main.cpp
@@ -0,0 +1,233 @@
+#include <GL/glew.h> // glew must be included before the main gl libs
+#include <GL/glut.h> // doing otherwise causes compiler shouting
+#include <iostream>
+
+//--Data types
+//This object will define the attributes of a vertex(position, color, etc...)
+struct Vertex
+{
+    GLfloat position[3];
+    GLfloat color[3];
+};
+
+//--Evil Global variables
+//Just for this example!
+int w = 640, h = 480;// Window size
+GLuint program;// The GLSL program handle
+GLuint vbo_geometry;// VBO handle for our geometry
+
+//attribute locations
+GLint loc_position;
+GLint loc_color;
+
+//--GLUT Callbacks
+void render();
+void update();
+void reshape(int n_w, int n_h);
+void keyboard(unsigned char key, int x_pos, int y_pos);
+
+//--Resource management
+bool initialize();
+void cleanUp();
+
+int main(int argc, char **argv)
+{
+    // Initialize glut
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(w, h);
+    // Name and create the Window
+    glutCreateWindow("Pass Through Shader Test");
+
+    // Now that the window is created the GL context is fully set up
+    // Because of that we can now initialize GLEW to prepare work with shaders
+    GLenum status = glewInit();
+    if( status != GLEW_OK)
+    {
+        std::cerr << "[F] GLEW NOT INITIALIZED: ";
+        std::cerr << glewGetErrorString(status) << std::endl;
+        return -1;
+    }
+
+    // Set all of the callbacks to GLUT that we need
+    glutDisplayFunc(render);// Called when its time to display
+    glutReshapeFunc(reshape);// Called if the window is resized
+    glutIdleFunc(update);// Called if there is nothing else to do
+    glutKeyboardFunc(keyboard);// Called if there is keyboard input
+
+    // Initialize all of our resources(shaders, geometry)
+    bool init = initialize();
+    if(init)
+    {
+        glutMainLoop();
+    }
+
+    // Clean up after ourselves
+    cleanUp();
+    return 0;
+}
+
+void render()
+{
+    //--Render the scene
+
+    //clear the screen
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //enable the shader program
+    glUseProgram(program);
+
+    //set up the Vertex Buffer Object so it can be drawn
+    glEnableVertexAttribArray(loc_position);
+    glEnableVertexAttribArray(loc_color);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_geometry);
+    //set pointers into the vbo for each of the attributes(position and color)
+    glVertexAttribPointer( loc_position,//location of attribute
+                           3,//number of elements
+                           GL_FLOAT,//type
+                           GL_FALSE,//normalized?
+                           sizeof(Vertex),//stride
+                           0);//offset
+
+    glVertexAttribPointer( loc_color,
+                           3,
+                           GL_FLOAT,
+                           GL_FALSE,
+                           sizeof(Vertex),
+                           (void*)offsetof(Vertex,color));
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);//mode, starting index, count
+
+    //clean up
+    glDisableVertexAttribArray(loc_position);
+    glDisableVertexAttribArray(loc_color);
+                           
+    //swap the buffers
+    glutSwapBuffers();
+}
+
+void update()
+{
+    // Update the state of the scene
+    glutPostRedisplay();//call the display callback
+}
+
+void reshape(int n_w, int n_h)
+{
+    // Change the Projection Matrix
+    // Wont be done this example
+}
+
+void keyboard(unsigned char key, int x_pos, int y_pos)
+{
+    // Handle keyboard input
+    if(key == 27)//ESC
+    {
+        exit(0);
+    }
+}
+
+bool initialize()
+{
+    // Initialize basic geometry and shaders for this example
+
+    //this defines a triangle, feel free to change it
+    // each row is first the position of the vertex then the color
+    Vertex geometry[] = { {{0.0, 0.9, 0.0}, {1.0, 0.0, 0.0}},
+                          {{-0.9, -0.9, 0.0}, {0.0, 1.0, 0.0}},
+                          {{0.9, -0.9, 0.0}, {0.0, 0.0, 1.0}}};
+    // Create a Vertex Buffer object to store this vertex info on the GPU
+    glGenBuffers(1, &vbo_geometry);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_geometry);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(geometry), geometry, GL_STATIC_DRAW);
+
+    //--Geometry done
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    //Shader Sources
+    // Put these into files and write a loader in the future
+    const char *vs =
+        "attribute vec3 v_position;"
+        "attribute vec3 v_color;"
+        "varying vec3 color;"
+        "void main(void){"
+        "   gl_Position = vec4(v_position, 1.0);"
+        "   color = v_color;"
+        "}";
+
+    const char *fs =
+        "varying vec3 color;"
+        "void main(void){"
+        "   gl_FragColor = vec4(color.xyz, 1.0);"
+        "}";
+
+    //compile the shaders
+    GLint shader_status;
+
+    // Vertex shader first
+    glShaderSource(vertex_shader, 1, &vs, NULL);
+    glCompileShader(vertex_shader);
+    //check the compile status
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &shader_status);
+    if(!shader_status)
+    {
+        std::cerr << "[F] FAILED TO COMPILE VERTEX SHADER!" << std::endl;
+        return false;
+    }
+
+    // Now the Fragment shader
+    glShaderSource(fragment_shader, 1, &fs, NULL);
+    glCompileShader(fragment_shader);
+    //check the compile status
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &shader_status);
+    if(!shader_status)
+    {
+        std::cerr << "[F] FAILED TO COMPILE FRAGMENT SHADER!" << std::endl;
+        return false;
+    }
+
+    //Now we link the 2 shader objects into a program
+    //This program is what is run on the GPU
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    //check if everything linked ok
+    glGetProgramiv(program, GL_LINK_STATUS, &shader_status);
+    if(!shader_status)
+    {
+        std::cerr << "[F] THE SHADER PROGRAM FAILED TO LINK" << std::endl;
+        return false;
+    }
+
+    //Now we set the locations of the attributes and uniforms
+    //this allows us to access them easily while rendering
+    loc_position = glGetAttribLocation(program,
+                    const_cast<const char*>("v_position"));
+    if(loc_position == -1)
+    {
+        std::cerr << "[F] POSITION NOT FOUND" << std::endl;
+        return false;
+    }
+
+    loc_color = glGetAttribLocation(program,
+                    const_cast<const char*>("v_color"));
+    if(loc_color == -1)
+    {
+        std::cerr << "[F] V_COLOR NOT FOUND" << std::endl;
+        return false;
+    }
+
+    //and its done
+    return true;
+}
+
+void cleanUp()
+{
+    // Clean up, Clean up
+    glDeleteProgram(program);
+    glDeleteBuffers(1, &vbo_geometry);
+}
