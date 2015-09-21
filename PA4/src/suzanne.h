44872777bfb343d3010b322df79ff0266849cf07
Saharath Kleips <SaharathKleips@yahoo.com>
2015-09-21 01:23:43 -0700

    trying to fix model loading

---
+++ PA4/src/suzanne.h
@@ -0,0 +1,34 @@
+#ifndef SUZANNE_H
+#define SUZANNE_H
+
+#include <QApplication>
+
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+#include <QDebug>
+
+#include <QVector3D>
+#include <QVector2D>
+#include <QVector>
+
+#include "renderable.h"
+#include "vertex.h"
+
+class Suzanne   :   public Renderable
+{
+public:
+    Suzanne();
+    ~Suzanne();
+
+    void initializeGL();
+    void paintGL( Camera3D& camera, QMatrix4x4& projection );
+    void teardownGL();
+
+private:
+    Vertex* loadObj( QString path );
+
+    Vertex* model;
+};
+
+#endif  //  SUZANNE_H
