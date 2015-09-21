f82be562477a7fa51002555ffc602792fefb2fb4
Saharath Kleips <SaharathKleips@yahoo.com>
2015-09-20 22:06:05 -0700

    init commit

---
+++ PA4/src/cube.cpp
@@ -0,0 +1,100 @@
+#include "cube.h"
+
+//
+// CONSTRUCTORS ////////////////////////////////////////////////////////////////
+// 
+
+/**
+ * @brief      Default constructor for Cube.
+ */
+Cube::Cube()
+{
+}
+
+/**
+ * @brief      Destructor for Cube.
+ */
+Cube::~Cube()
+{
+    this->teardownGL();
+}
+
+//
+// INTERFACE IMPLEMENTATIONS ///////////////////////////////////////////////////
+// 
+
+
+void Cube::initializeGL()
+{
+    initializeOpenGLFunctions();
+
+    // Create the Shader for the Cube to use
+    program = new QOpenGLShaderProgram();
+    program->addShaderFromSourceFile( QOpenGLShader::Vertex, 
+        ":/shaders/simple.vs" );
+    program->addShaderFromSourceFile( QOpenGLShader::Fragment,
+        ":/shaders/simple.fs" );
+    program->link();
+    program->bind();
+
+    // Cache the Uniform Locations
+    modelWorld = program->uniformLocation( "model_to_world" );
+    worldEye = program->uniformLocation( "world_to_eye" );
+    eyeClip = program->uniformLocation( "eye_to_clip" );
+
+    // Create a Vertex Buffer Object (vbo)
+    vbo.create();
+    vbo.bind();
+    vbo.setUsagePattern( QOpenGLBuffer::StaticDraw );
+    vbo.allocate( sg_vertices, sizeof( sg_vertices ) );
+
+    // Create a Vertex Array Object (vao)
+    vao.create();
+    vao.bind();
+    program->enableAttributeArray( 0 );
+    program->enableAttributeArray( 1 );
+    program->setAttributeBuffer(    0,
+                                    GL_FLOAT,
+                                    Vertex::positionOffset(),
+                                    Vertex::PositionTupleSize,
+                                    Vertex::stride() );
+    program->setAttributeBuffer(    1,
+                                    GL_FLOAT,
+                                    Vertex::colorOffset(),
+                                    Vertex::ColorTupleSize,
+                                    Vertex::stride() );
+
+    // Release all (order matters)
+    vao.release();
+    vbo.release();
+    program->release();
+}
+
+void Cube::paintGL( Camera3D& camera, QMatrix4x4& projection )
+{
+    glEnable( GL_DEPTH_TEST );
+    glDepthFunc( GL_LEQUAL );
+    glDepthMask( GL_TRUE );
+    glEnable( GL_CULL_FACE );
+
+    program->bind();
+
+    program->setUniformValue( worldEye, camera.toMatrix() );
+    program->setUniformValue( eyeClip, projection );
+
+    vao.bind();
+    program->setUniformValue( modelWorld, transform.toMatrix() );
+    glDrawArrays(   GL_TRIANGLES,
+                    0,
+                    sizeof( sg_vertices ) / sizeof( sg_vertices[0] ) );
+    vao.release();
+
+    program->release();
+}
+
+void Cube::teardownGL()
+{
+    vao.destroy();
+    vbo.destroy();
+    delete program;
+}
---
+++ PA4/src/oglWidget.h
@@ -0,0 +1,52 @@
+#ifndef OGL_WIDGET_H
+#define OGL_WIDGET_H
+
+#include <QApplication>
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+
+#include <QMatrix4x4>
+
+#include <QKeyEvent>
+
+#include <QDebug>
+#include <QString>
+
+#include "camera3d.h"
+#include "renderable.h"
+#include "cube.h"
+
+class OGLWidget    :    public QOpenGLWidget,
+                        protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    OGLWidget();
+    ~OGLWidget();
+
+    virtual void initializeGL();
+    virtual void resizeGL( int width, int height );
+    virtual void paintGL();
+    virtual void teardownGL();
+
+protected slots:
+    void update();
+
+signals:
+    void exitFlag();
+
+protected:
+    void keyPressEvent( QKeyEvent* event );
+
+private:
+    void printContextInfo();
+
+    Renderable* cube;
+
+    QMatrix4x4 projection;
+    Camera3D camera;
+};
+
+#endif  //  OGL_WIDGET_H
