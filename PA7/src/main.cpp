7fc67ae28058349f66cb8fae817e36d7c9c04e49
zarol <saharathkleips@yahoo.com>
2015-10-08 19:52:07 -0700

    added main drivers

--- PA7/src/main.cpp
+++ PA7/src/main.cpp
@@ -5,6 +5,7 @@ int main( int argc, char** argv )
 {
     QApplication app( argc, argv );
 
+    // Set the drawing format
     QSurfaceFormat format;
     format.setDepthBufferSize( 24 );
     format.setSwapBehavior( QSurfaceFormat::DoubleBuffer );
@@ -12,6 +13,7 @@ int main( int argc, char** argv )
     format.setProfile( QSurfaceFormat::NoProfile );
     format.setVersion( 2,1 );
 
+    // Create the main OpenGL program
     OGLWidget* oglWidget;
     oglWidget = new OGLWidget();
     oglWidget->setFormat( format );
