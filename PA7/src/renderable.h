6e9bcc3f6fc044151e59c167a78a8e3c9036d0af
zarol <saharathkleips@yahoo.com>
2015-10-09 01:38:45 -0700

    added renderable interface

---
+++ PA7/src/renderable.h
@@ -0,0 +1,16 @@
+#ifndef RENDERABLE_H
+#define RENDERABLE_H
+
+#include <QOpenGLFunctions>
+#include <QMatrix4x4>
+#include "camera3d.h"
+
+class Renderable    :   protected QOpenGLFunctions
+{
+public:
+    virtual void initializeGL() = 0;
+    virtual void paintGL( Camera3D& camera, QMatrix4x4& projection ) = 0;
+    virtual void teardownGL() = 0;
+};
+
+#endif  //  RENDERABLE
