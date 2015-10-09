928c773ad68857ab4b0510db545f4948e288f42b
zarol <saharathkleips@yahoo.com>
2015-10-08 19:37:55 -0700

    added oglWidget

--- PA7/src/main.cpp
+++ PA7/src/main.cpp
@@ -1,4 +1,24 @@
+#include <QApplication>
+#include "oglWidget.h"
+
 int main( int argc, char** argv )
 {
-    return 0;
+    QApplication app( argc, argv );
+
+    QSurfaceFormat format;
+    format.setDepthBufferSize( 24 );
+    format.setSwapBehavior( QSurfaceFormat::DoubleBuffer );
+    format.setRenderableType( QSurfaceFormat::OpenGL );
+    format.setProfile( QSurfaceFormat::NoProfile );
+    format.setVersion( 2,1 );
+
+    OGLWidget* oglWidget;
+
+    oglWidget = new OGLWidget();
+
+    oglWidget->setFormat( format );
+    oglWidget->resize( QSize( 800, 600 ) );
+    oglWidget->show(); 
+
+    return app.exec();
 }
