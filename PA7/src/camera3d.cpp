d66ce83f8ea3d1c216fd2930e23a2887b02b7f52
zarol <saharathkleips@yahoo.com>
2015-10-10 23:44:39 -0700

    fixed LocalForward

--- PA7/src/camera3d.cpp
+++ PA7/src/camera3d.cpp
@@ -1,6 +1,6 @@
 #include "camera3d.h"
 
-const QVector3D Camera3D::LocalForward( 0.0f, 0.0f, 1.0f );
+const QVector3D Camera3D::LocalForward( 0.0f, 0.0f, -1.0f );
 const QVector3D Camera3D::LocalUp( 0.0f, 1.0f, 0.0f );
 const QVector3D Camera3D::LocalRight( 1.0f, 0.0f, 0.0f );
 
