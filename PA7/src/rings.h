903698c3fa75e0d47ac40709a2c22c3aba1f521f
Denis Morozov <morozovsdenis@gmail.com>
2015-10-18 22:19:07 -0700

    Created rings.h

---
+++ PA7/src/rings.h
@@ -0,0 +1,59 @@
+#ifndef RINGS_H
+#define RINGS_H
+
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLTexture>
+#include <QOpenGLShaderProgram>
+#include <QDebug>
+
+#include "../renderable.h"
+#include "../modelLoader.h"
+#include "../transform3d.h"
+#include "../vertex.h"
+
+class QOpenGLShaderProgram;
+
+class Rings    :   public Renderable,
+                    protected ModelLoader
+{
+protected:
+    Rings();
+    Rings( QString texturePath );
+    ~Rings();
+
+public:
+    void initializeGL();
+    void paintGL( Camera3D& camera, QMatrix4x4& projection );
+    virtual void update();
+    void teardownGL();
+
+    // Rings Information
+    Transform3D transform;
+    static bool SCALED;
+
+private:
+    // OpenGL State Information
+    static QOpenGLBuffer* vbo;
+    static QOpenGLVertexArrayObject* vao;
+    static QOpenGLShaderProgram* program;
+
+    // Model Information
+    const QString MODEL_PATH = "models/planet.obj";
+    static Vertex* model;
+    static int numVertices;
+
+    // Texture Information
+    QString m_texturePath;
+    QOpenGLTexture* texture;
+
+    // Shader Information
+    const QString V_SHADER_PATH = ":/shader/simple.vs";
+    const QString F_SHADER_PATH = ":/shader/simple.fs";
+    static int modelWorld;
+    static int worldEye;
+    static int eyeClip;
+};
+
+#endif  //  RINGS_H
