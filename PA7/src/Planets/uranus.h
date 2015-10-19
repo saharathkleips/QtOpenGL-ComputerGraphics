5e9e1cd8dbbe90e94450de1a36d614e33bac6049
Denis Morozov <morozovsdenis@gmail.com>
2015-10-18 23:36:49 -0700

    Uranus now creates its ring

--- PA7/src/Planets/uranus.h
+++ PA7/src/Planets/uranus.h
@@ -2,13 +2,22 @@
 #define URANUS_H
 
 #include "planet.h"
+#include "../rings.h"
 
 class Uranus     :   public Planet
 {
 public:
     Uranus();
 
+    void initializeGL();
+    void paintGL( Camera3D& camera, QMatrix4x4& projection );
     void update();
+    void teardownGL();
+
+private:
+    void updateReal();
+    void updateScaled();
+    Rings *ring;
 };
 
 #endif  //  URANUS_H
