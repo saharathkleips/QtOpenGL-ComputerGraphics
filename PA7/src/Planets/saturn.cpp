4a3d47eccfbab25d9f18a5fc5e39c6b46c827874
Denis Morozov <morozovsdenis@gmail.com>
2015-10-18 21:59:54 -0700

    Saturn can now hold rings

--- PA7/src/Planets/saturn.cpp
+++ PA7/src/Planets/saturn.cpp
@@ -14,7 +14,43 @@ Saturn::Saturn()
 // OVERRIDDEN FUNCTIONS
 // 
 
+void Saturn::initializeGL()
+{
+    Planet::initializeGL();
+}
+
+
+void Saturn::paintGL( Camera3D& camera, QMatrix4x4& projection )
+{
+    Planet::paintGL( camera, projection );
+}
+
 void Saturn::update()
 {
-    transform.rotate( 1.0f, 0, 1, 0 );
+    if( Planet::SCALED )
+        updateScaled();
+    else
+        updateReal();   
+}
+
+void Saturn::teardownGL()
+{
+    Planet::teardownGL();
+}
+
+//
+// PRIVATE HELPER FUNCTIONS ////////////////////////////////////////////////////
+//
+
+void Saturn::updateScaled()
+{
+    // SATURN
+    {
+        transform.rotate( 1.0f, 0, 1, 0 );
+    }
+}
+
+void Saturn::updateReal()
+{
+
 }
--- PA7/src/Planets/saturn.h
+++ PA7/src/Planets/saturn.h
@@ -8,7 +8,14 @@ class Saturn     :   public Planet
 public:
     Saturn();
 
+    void initializeGL();
+    void paintGL( Camera3D& camera, QMatrix4x4& projection );
     void update();
+    void teardownGL();
+
+private:
+    void updateReal();
+    void updateScaled();
 };
 
 #endif  //  SATURN_H
