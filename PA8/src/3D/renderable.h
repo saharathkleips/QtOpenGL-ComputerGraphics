878ec82a256149bf9bc9e7e0f6239bd239f4b85f
zarol <saharathkleips@yahoo.com>
2015-10-20 12:14:37 -0700

    moved Transform3D to renderable

--- PA8/src/3D/renderable.h
+++ PA8/src/3D/renderable.h
@@ -4,6 +4,7 @@
 #include <QOpenGLFunctions>
 #include <QMatrix4x4>
 #include "Controls/camera3d.h"
+#include "3D/transform3d.h"
 
 class Renderable    :   protected QOpenGLFunctions
 {
@@ -12,6 +13,8 @@ public:
     virtual void paintGL( Camera3D& camera, QMatrix4x4& projection ) = 0;
     virtual void update() = 0;
     virtual void teardownGL() = 0;
+
+    Transform3D Transform;
 };
 
 #endif  //  RENDERABLE
