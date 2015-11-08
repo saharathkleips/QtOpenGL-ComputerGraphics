57d4a58a72a9804fbebf87c5798b51f259445c39
zarol <saharathkleips@yahoo.com>
2015-11-07 22:03:08 -0800

    Made constructor less scary

--- PA9/src/GameObjects/hockeyPaddle.cpp
+++ PA9/src/GameObjects/hockeyPaddle.cpp
@@ -1,18 +1,18 @@
 #include "hockeyPaddle.h"
 
 
-HockeyPaddle::HockeyPaddle( const QString &color )
-    :   ColorPhysicsEntity( 
-            btTransform( 
-                btQuaternion( 0, 0, 0, 1 ), 
-                [&, color]{ 
-                    return color == "Red" ? btVector3( -5, 50, 0 ) : btVector3( 5, 50, 0 );
-                }()
-            ),
-            1, 
-            [&, color]{ 
-                return color == "Red" ? "models/Paddle250r.obj" : "models/Paddle250b.obj";
-            }()
-        )
+HockeyPaddle::HockeyPaddle( const QString &color ) : ColorPhysicsEntity( 
+    btTransform( 
+        btQuaternion( 0, 0, 0, 1 ), 
+        [&, color]{ 
+            return color == "Red" ? btVector3( -25, 50, 0 ) : btVector3( 25, 50, 0 );
+        }()
+    ),
+    1, 
+    [&, color]{ 
+        return color == "Red" ? "models/Paddle250r.obj" : "models/Paddle250b.obj";
+    }()
+)
+
 {
 }
