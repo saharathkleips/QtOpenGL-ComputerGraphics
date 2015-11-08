f759be25185fd65ac69d1d5ae78e38372ead7084
bennicholes <nicholes.ben@gmail.com>
2015-11-07 22:50:40 -0800

    added sound for hockey table

--- PA9/src/GameObjects/hockeyTable.h
+++ PA9/src/GameObjects/hockeyTable.h
@@ -1,12 +1,20 @@
 #ifndef HOCKEY_TABLE_H
 #define HOCKEY_TABLE_H
 
+#include <QMediaPlayer>
 #include "Entity/colorPhysicsEntity.h"
+
 
 class HockeyTable	:	public ColorPhysicsEntity
 {
 public:
 	HockeyTable();
+	void update();
+
+private:
+	QMediaPlayer *player;
 };
+
+
 
 #endif	//	HOCKEY_TABLE_H
