9180582e5d9ed6757bce2309a225e3ab00ea9611
Zarol <saharathkleips@nevada.unr.edu>
2015-11-07 18:48:43 -0800

    added hockey table

---
+++ PA9/src/GameObjects/hockeyTable.cpp
@@ -0,0 +1,6 @@
+#include "hockeyTable.h"
+
+HockeyTable::HockeyTable()
+	:	ColorEntity( "models/HockeyTable.obj" )
+{
+}
---
+++ PA9/src/GameObjects/hockeyTable.h
@@ -0,0 +1,12 @@
+#ifndef HOCKEY_TABLE_H
+#define HOCKEY_TABLE_H
+
+#include "Entity/colorEntity.h"
+
+class HockeyTable	:	public ColorEntity
+{
+public:
+	HockeyTable();
+};
+
+#endif	//	HOCKEY_TABLE_H
