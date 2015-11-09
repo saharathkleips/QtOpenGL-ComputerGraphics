e8834ad9394af6a74cb96bef142281d39829a2dd
bennicholes <nicholes.ben@gmail.com>
2015-11-08 16:42:27 -0800

    change default sound level

--- PA9/src/GameObjects/hockeyTable.cpp
+++ PA9/src/GameObjects/hockeyTable.cpp
@@ -6,7 +6,7 @@ HockeyTable::HockeyTable()
 {
 	player = new QMediaPlayer(); 
     player->setMedia(QUrl::fromLocalFile("/home/ben/Desktop/cs480Kleips/PA9/src/Sounds/hum.mp3"));    
-    player->setVolume(50);	
+    player->setVolume(800);	
     player->play();
 }
 
