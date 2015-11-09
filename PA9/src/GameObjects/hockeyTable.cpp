a6e9bba9bf58e38809f3388fc97665d847f8ef90
bennicholes <nicholes.ben@gmail.com>
2015-11-09 00:20:05 -0800

    table sounds

--- PA9/src/GameObjects/hockeyTable.cpp
+++ PA9/src/GameObjects/hockeyTable.cpp
@@ -1,11 +1,15 @@
 #include "hockeyTable.h"
+#include <QFileInfo>
 
 HockeyTable::HockeyTable()
 	:	ColorPhysicsEntity( btTransform( btQuaternion( 0, 0, 0, 1 ), 
 		btVector3( 0, 0, 0 ) ), 0, "models/HockeyTable.obj" )
 {
 	player = new QMediaPlayer(); 
-    player->setMedia(QUrl::fromLocalFile("/home/ben/Desktop/cs480Kleips/PA9/src/Sounds/hum.mp3"));    
+    // player->setMedia(QUrl::fromLocalFile("/home/ben/Desktop/cs480Kleips/PA9/src/Sounds/hum.mp3"));
+    const QString file = "sounds/hum.mp3";
+    QUrl url = QUrl::fromLocalFile(QFileInfo(file).absoluteFilePath());
+    player->setMedia(url);
     player->setVolume(800);	
     player->play();
 }
