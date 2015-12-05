fdfce74dde975d5ffb1233c16cf06f9abd033d4f
zarol <saharathkleips@yahoo.com>
2015-12-05 13:31:28 -0800

    Created blocks based on maze

--- PA11/src/Maze/labyrinth.cpp
+++ PA11/src/Maze/labyrinth.cpp
@@ -15,8 +15,39 @@ void Labyrinth::addRigidBodys( btDiscreteDynamicsWorld* dynamicsWorld )
 
 void Labyrinth::initializeGL()
 {
-    m_cells.push_back( new Cell( btTransform( btQuaternion( 0, 0, 0, 1 ), 
-            btVector3( 5.0, 5.0, 5.0 ) ), "textures/Cube_RockWall.jpg" ) );
+    float xpos = 0.0f, zpos = 0.0f;
+    // m_cells.push_back( new Cell( btTransform( btQuaternion( 0, 0, 0, 1 ), 
+    //     btVector3( xpos, 0.0f, zpos ) ), "textures/Cube_RockWall.jpg" ) );
+    // m_cells.push_back( new Cell( btTransform( btQuaternion( 0, 0, 0, 1 ), 
+    //     btVector3( 2.0f, 0.0f, 2.0f ) ), "textures/Cube_RockWall.jpg" ) );
+    for( unsigned int y = 0; y < m_maze[0].size(); ++y )
+    {
+        for( unsigned int x = 0; x < m_maze.size(); ++x )
+        {
+            if( m_maze[x][y] == MazeGenerator::WALL )
+            {
+                m_cells.push_back( new Cell( btTransform( btQuaternion( 0, 0, 0, 1 ), 
+                    btVector3( xpos, 2.0f, zpos ) ), "textures/Cube_RockWall.jpg" ) );
+            }
+            else if ( m_maze[x][y] == MazeGenerator::FLOOR )
+            {
+                m_cells.push_back( new Cell( btTransform( btQuaternion( 0, 0, 0, 1 ), 
+                    btVector3( xpos, 0.0f, zpos ) ), "textures/Cube_DirtFloor.jpg" ) );
+            }
+            xpos += 2.0f;
+        }
+        xpos = 0.0f;
+        zpos += 2.0f;
+    }
+
+    // // North
+    // for( float i = -2.0f; i <= m_maze.size() * 2.0f; i += 2.0f )
+    //     m_cells.push_back( new Cell( btTransform( btQuaternion( 0, 0, 0, 1 ), 
+    //                 btVector3( i, 2.0f, -2.0f ) ), "textures/Cube_RockWall.jpg" ) );
+    // // South
+    // for( float i = -2.0f; i <= m_maze.size() * 2.0f; i += 2.0f )
+    //     m_cells.push_back( new Cell( btTransform( btQuaternion( 0, 0, 0, 1 ), 
+    //                 btVector3( i, 2.0f, m_maze[0].size() * 2.0f ) ), "textures/Cube_RockWall.jpg" ) );
 
     for( Cell* cell : m_cells )
         cell->initializeGL();
